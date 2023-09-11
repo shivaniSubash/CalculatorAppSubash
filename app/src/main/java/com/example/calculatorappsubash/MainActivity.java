3d0fcab1318d7b93a09c0b1f18fc7f2927cd96d6
shivaniSubash <birdiesrus1234@gmail.com>
2023-09-11 11:04:21 -0500

    created mult method in MainActivity

--- app/src/main/java/com/example/calculatorappsubash/MainActivity.java
+++ app/src/main/java/com/example/calculatorappsubash/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorappsubash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    EditText num1ET = findViewById(R.id.editTextNumberInputBox);
+    String strNum1 = num1ET.getText().toString();
+    int num1= Integer.valueOf(strNum1);
+    EditText num2ET = findViewById(R.id.editTextNumberInputBox2);
+    String strNum2 = num2ET.getText().toString();
+    int num2 = Integer.valueOf(strNum2);
+    public void mult(){
+       int output= num1*num2;
     }
 }
